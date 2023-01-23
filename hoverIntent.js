2107ed1cbdac7873cfde759374d48e8098ea6471
Timofey Petrikevich <petrik3003@gmail.com>
2023-01-23 23:29:43 +0300

    ref: () => instead of class functions

--- hoverIntent.js
+++ hoverIntent.js
@@ -3,7 +3,6 @@
 // Здесь показан набросок класса
 // с возможностями, которые нам понадобятся
 class HoverIntent {
-
   constructor({
     sensitivity = 0.1, // скорость ниже 0.1px/ms значит "курсор на элементе"
     interval = 100, // измеряем скорость каждые 100ms: определяем дистанцию между предыдущей и новой позицией.
@@ -31,87 +30,78 @@ class HoverIntent {
     this.overElem = false;
 
     // убедитесь, что "this" сохраняет своё значение для обработчиков.
-    this.onMouseMove = this.onMouseMove.bind(this);
-    this.onMouseOver = this.onMouseOver.bind(this);
-    this.onMouseOut = this.onMouseOut.bind(this);
-    this.trackSpeed = this.trackSpeed.bind(this);
+    this.onMouseMove = (event) => {
+      this.lastClientX = event.clientX;
+      this.lastClientY = event.clientY;
+      this.lastTime = Date.now();
+    }
+
+    this.onMouseOver = (event) => {
+      if (this.onElem) {
+        return;
+      }
+
+      this.onElem = true;
+
+      this.prevClientX = event.clientX;
+      this.prevClientY = event.clientY;
+      this.prevTime = Date.now();
+
+      this.elem.addEventListener("mousemove", this.onMouseMove);
+      this.checkSpdInterval = setInterval(this.trackSpeed, this.interval);
+    }
+
+    this.trackSpeed = () => {
+      let spd;
+
+      if (!this.lastTime || this.lastTime == this.prevTime) {
+        spd = 0;
+      } else {
+        let xMove = this.lastClientX - this.prevClientX;
+        let yMove = this.lastClientY - this.prevClientY;
+        let dist = Math.sqrt(Math.pow(xMove, 2) + Math.pow(yMove, 2));
+
+        let time = this.lastTime - this.prevTime;
+
+        spd = dist / time;
+      }
+
+      if (spd < this.sensitivity) {
+        this.overElem = true;
+        clearInterval(this.checkSpdInterval);
+        this.over.call(this.elem);
+      } else {
+        this.prevClientX = this.lastClientX;
+        this.prevClientY = this.lastClientY;
+        this.prevTime = this.lastTime;
+      }
+    }
+
+    this.onMouseOut = (event) => {
+      let toElement = event.relatedTarget;
+      if (toElement == this.elem || this.elem.contains(toElement)) {
+        return;
+      }
+
+      this.onElem = false;
+      this.elem.removeEventListener("mousemove", this.onMouseMove);
+      clearInterval(this.checkSpdInterval);
+
+      if (this.overElem) {
+        this.overElem = false;
+        this.out.call(this.elem);
+      }
+    }
+
+    this.destroy = () => {
+      this.elem.removeEventListener("mouseover", this.onMouseOver);
+      this.elem.removeEventListener("mouseout", this.onMouseOut);
+      this.elem.removeEventListener("mousemove", this.onMouseMove);
+    }
 
     // назначаем обработчики
     elem.addEventListener("mouseover", this.onMouseOver);
     elem.addEventListener("mouseout", this.onMouseOut);
-
-    // продолжите с этого места
-
-  }
-
-  onMouseOver(event) {
-    if (this.onElem) {
-      return;
-    }
-
-    this.onElem = true;
-
-    this.prevClientX = event.clientX;
-    this.prevClientY = event.clientY;
-    this.prevTime = Date.now();
-
-    this.elem.addEventListener("mousemove", this.onMouseMove);
-    this.checkSpdInterval = setInterval(this.trackSpeed, this.interval);
-  }
-
-  trackSpeed() {
-    let spd;
-
-    if (!this.lastTime || this.lastTime == this.prevTime) {
-      spd = 0;
-    } else {
-      let xMove = this.lastClientX - this.prevClientX;
-      let yMove = this.lastClientY - this.prevClientY;
-      let dist = Math.sqrt(Math.pow(xMove, 2) + Math.pow(yMove, 2));
-
-      let time = this.lastTime - this.prevTime;
-
-      spd = dist / time;
-    }
-
-    if (spd < this.sensitivity) {
-      this.overElem = true;
-      clearInterval(this.checkSpdInterval);
-      this.over.call(this.elem);
-    } else {
-      this.prevClientX = this.lastClientX;
-      this.prevClientY = this.lastClientY;
-      this.prevTime = this.lastTime;
-    }
-  }
-
-  onMouseOut(event) {
-    let toElement = event.relatedTarget;
-    if (toElement == this.elem || this.elem.contains(toElement)) {
-      return;
-    }
-
-    this.onElem = false;
-    this.elem.removeEventListener("mousemove", this.onMouseMove);
-    clearInterval(this.checkSpdInterval);
-
-    if (this.overElem) {
-      this.overElem = false;
-      this.out.call(this.elem);
-    }
-  }
-
-  onMouseMove(event) {
-    this.lastClientX = event.clientX;
-    this.lastClientY = event.clientY;
-    this.lastTime = Date.now();
-  }
-
-
-  destroy() {
-    this.elem.removeEventListener("mouseover", this.onMouseOver);
-    this.elem.removeEventListener("mouseout", this.onMouseOut);
-    this.elem.removeEventListener("mousemove", this.onMouseMove);
   }
 
 }
