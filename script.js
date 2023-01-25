48d45e1a14161e1da204d022936b33ef99569170
Timofey Petrikevich <petrik3003@gmail.com>
2023-01-25 23:54:37 +0300

    feat: image loading

--- script.js
+++ script.js
@@ -2,8 +2,30 @@
      * Проверка видимости элемента (в видимой части страницы)
      * Достаточно, чтобы верхний или нижний край элемента был виден
      */
-function isVisible(elem) {
-    // ...ваш код...
+function isVisible(elem, vBuffer = 0, hBuffer = 0) {
+    let bbox = elem.getBoundingClientRect();
+
+    let wWidth = document.documentElement.clientWidth;
+    let wHeight = document.documentElement.clientHeight;
+
+    if (bbox.left > wWidth + hBuffer) {
+        return false;
+    }
+
+    if (bbox.right < -hBuffer) {
+        return false;
+    }
+
+    if (bbox.top > wHeight + vBuffer) {
+        return false;
+    }
+
+    if (bbox.bottom < -vBuffer) {
+        return false;
+    }
+
+    return true;
+
 }
 
 function showVisible() {
@@ -17,6 +39,7 @@ function showVisible() {
             realSrc += '?nocache=' + Math.random();
 
             img.src = realSrc;
+            img.classList.add('loaded');
 
             img.dataset.src = '';
         }
