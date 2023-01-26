205d7516015772dd306c6ac4bf1c625a848749e0
Timofey Petrikevich <petrik3003@gmail.com>
2023-01-26 16:04:34 +0300

    feat: div/textarea

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 // ...ваш код...
 // Заметьте: <textarea> должен иметь class="edit"
 // my.css содержит стиль, чтобы <textarea> и <div> были одного размера
+
+let edit = document.getElementById('view');
+
+function editToggleTextArea() {
+    let textArea = document.createElement('textarea');
+    textArea.value = edit.innerHTML;
+    textArea.classList.add('edit');
+    textArea.id = "view";
+    edit.replaceWith(textArea);
+    edit = textArea;
+    edit.addEventListener("blur", editToggleDiv);
+    edit.addEventListener('keydown', (event) => {
+        if (event.key != "Enter") {
+            return;
+        }
+        edit.blur();
+    });
+}
+
+function editToggleDiv() {
+    let div = document.createElement('div');
+    div.innerHTML = edit.value;
+    div.classList.add('view');
+    div.id = "view";
+    edit.replaceWith(div);
+    edit = div;
+    edit.addEventListener('pointerdown', editToggleTextArea);
+}
+
+edit.addEventListener('pointerdown', editToggleTextArea);
