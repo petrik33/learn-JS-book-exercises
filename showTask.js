b231b274568ec11edf1124e108c1c1137a7e7285
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-03-06 16:01:27 +0300

    fix: show task

--- showTask.js
+++ showTask.js
@@ -1,15 +1,15 @@
-import { marked } from "marked";
-
 const getFileText = async (file) => {
   const response = await fetch(file);
   const text = await response.text();
   return text;
 }
 
-const parseMarkDown = (text) => {
-  return marked.parse(text);
+const readTask = async (taskPath) => {
+  const text = await getFileText(taskPath);
+  task.innerHTML = marked.parse(text);
 }
 
 const task = document.getElementById('task');
 const taskPath = task.dataset['path'];
-task.innerHTML = parseMarkDown(getFileText(taskPath));
+
+(async () => await readTask(taskPath))();
